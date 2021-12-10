b7f72d8b921b2b7945392f875138a4b6e5cf22e3
Theodore <theodore.daniel@epitech.eu>
2021-12-10 19:06:56 +0100

    Adding player, enemy, and basic player controls

--- src/Actors/actor.gd
+++ src/Actors/actor.gd
@@ -3,8 +3,4 @@ class_name Actor
 
 export var gravety: = 10.0
 export var max_speed: = Vector2(300.0, 300.0)
-var velocity: = Vector2.ZERO
-
-func _physics_process(delta):
-	velocity.y += gravety * delta
-	velocity = move_and_slide(velocity)
+var _velocity: = Vector2.ZERO
