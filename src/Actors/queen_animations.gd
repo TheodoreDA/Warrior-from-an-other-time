b403b59ad26af1ca7e2774bc0f8041226f771046
Theodore <theodore.daniel@epitech.eu>
2021-12-11 17:28:52 +0100

    Adding basic animation for the queen

---
+++ src/Actors/queen_animations.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite
+
+func _physics_process(delta):
+	if Input.get_action_strength("move_right"):
+		flip_h = false
+	if Input.get_action_strength("move_left"):
+		flip_h = true
+
+	if Input.get_action_strength("move_right"):
+		self.animation = "walk"
+	if Input.get_action_strength("move_left"):
+		self.animation = "walk"
+	if Input.is_action_just_released("move_left"):
+		self.animation = "idle"
+	if Input.is_action_just_released("move_right"):
+		self.animation = "idle"
+		
