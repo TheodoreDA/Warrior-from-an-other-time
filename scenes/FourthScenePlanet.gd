124e1e7184d8c51acdb016c9d383c05c52ea0e86
Arthur <arthur.croix@epitech.eu>
2021-12-12 15:55:07 +0100

    Last scene finished

--- scenes/FourthScenePlanet.gd
+++ scenes/FourthScenePlanet.gd
@@ -12,7 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_node("RichTextLabel2").set_position(Vector2(get_node("queen").position.x, get_node("RichTextLabel2").get_position().y))
+	get_node("RichTextLabel2").set_position(Vector2(get_node("queen").position.x - 100, get_node("RichTextLabel2").get_position().y))
 	if (get_node("queen").position.x > 2750 && !hasBeenPlayed):
 		get_node("AnimationPlayer").play("Cutscene")
 		hasBeenPlayed = true
